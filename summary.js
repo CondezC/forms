8fe7fda5d65f3f7bf772c7bbc0d5916a9d8e5114
Carylle Jane Condez <caryllejane.condez@gmail.com>
2026-03-16 14:47:09 +0800

    Fixed js

--- summary.js
+++ summary.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCfQTN7UTRcW9lBm-rhRqyAOcT5gFZDJIs",
-    authDomain: "sss-qr-system-39b59.firebaseapp.com",
-    projectId: "sss-qr-system-39b59",
-    storageBucket: "sss-qr-system-39b59.firebasestorage.app",
-    messagingSenderId: "81090026028",
-    appId: "1:81090026028:web:91dfd833462e2d95434f89"
+  apiKey: "AIzaSyCfQTN7UTRcW9lBm-rhRqyAOcT5gFZDJIs",
+  authDomain: "sss-qr-system-39b59.firebaseapp.com",
+  projectId: "sss-qr-system-39b59",
+  storageBucket: "sss-qr-system-39b59.firebasestorage.app",
+  messagingSenderId: "81090026028",
+  appId: "1:81090026028:web:91dfd833462e2d95434f89"
 };
 
 firebase.initializeApp(firebaseConfig);
